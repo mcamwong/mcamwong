66ad9538e1dd35a7e5c881ddb6227b77dc1efd8b
mcamwong <62860733+mcamwong@users.noreply.github.com>
2020-03-30 11:39:27 +1100

    Update Baseline generator.py

--- Baseline generator.py
+++ Baseline generator.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[80]:
-
-
 # BEFORE STARTING GO TO .csv file and
 # delete all column text e.g timestamp except for Unit and V or will be confusing for datafile to be read
 # should only be 2 columns, and Unit , V at top, then all else numbers.
@@ -17,11 +14,6 @@ df = pd.read_csv(r"C:\Users\Marcus\Downloads\PT40A_M000_10mVperpC_2Hz_10N.csv")
                 #replace this with your data file location in your comp
 print(df)
 
-
-
-# In[81]:
-
-
 plt.plot(df.Unit, df.V)
 plt.ylabel("V")
 plt.xlabel("Unit")
@@ -29,17 +21,9 @@ plt.show()
 #prints data
 
 
-# In[82]:
-
-
 x = df['V']
 
 print(x)
-
-#extract V column
-
-
-# In[83]:
 
 
 plt.plot(x)
@@ -50,9 +34,6 @@ plt.show()
 # plots data file, V only
 
 
-# In[84]:
-
-
 flipx = -x
 plt.plot(-x)
 plt.ylabel("Flipped V")
@@ -60,8 +41,6 @@ plt.xlabel("Row number")
 plt.show()
 # I flip X because it only lets me find peaks not valleys, main thing is we find the row no. of each peak
 
-
-# In[85]:
 
 
 peaks, _ = find_peaks(flipx, distance=10e4, height = -0.9, prominence = 0 )
@@ -73,7 +52,7 @@ print(peaks)
 #if you want to only pick big peaks from the base i.e remove those small ones at start and end, increase prominence
 
 
-# In[86]:
+
 
 
 plt.plot(x)
@@ -84,7 +63,7 @@ plt.show()
 #shows the peaks highlighted in the original, unflipped version, view this to make sure all orange dots are in areas you like
 
 
-# In[87]:
+
 
 
 rowpick = peaks
@@ -93,7 +72,7 @@ finaldata = df.loc[rowpick]
 print(finaldata)
 
 
-# In[88]:
+
 
 
 plt.plot(df.Unit, df.V)
@@ -104,7 +83,7 @@ plt.show()
 #plots the baseline as a new graph for visual check
 
 
-# In[89]:
+
 
 
 finaldata.to_csv(r'C:\Users\Marcus\Downloads\baseline.csv', index = False)
@@ -112,7 +91,7 @@ finaldata.to_csv(r'C:\Users\Marcus\Downloads\baseline.csv', index = False)
 #exports data as csv change the path to where you want to download
 
 
-# In[ ]:
+
 
 
 
